9e7c5bba981cf2d609fb1dd200d42e0c3ad79833
vismaya01 <kvvismaya98@gmail.com>
2020-11-27 22:07:27 +0530

    Refactored: Used Generic class for finding maximun

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,6 +1,20 @@
 
-public class FindMaximum {
-	public <E extends Comparable<E>> E findMax(E first, E second, E thrid) {
+public class FindMaximum<T extends Comparable<T>> {
+	T first;
+	T second;
+	T thrid;
+	
+	public FindMaximum(T first, T second, T third) {
+		this.first = first;
+		this.second = second;
+		this.thrid = third;
+	}
+	
+	public T findMax() {
+		return FindMaximum.findMax(this.first, this.second, this.thrid);
+	}
+	
+	public static <E extends Comparable<E>> E findMax(E first, E second, E thrid) {
 		E max = first;
 		if(second.compareTo(max) > 0)
 			max = second;
--- src/FindMaximumTest.java
+++ src/FindMaximumTest.java
@@ -1,73 +1,72 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
 import org.junit.Assert;
 
 public class FindMaximumTest {
 
 	@Test
 	public void givenMaxNumberAtFirstPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		Integer result = findMax.findMax(9, 3, 5);
+		FindMaximum<Integer> findMax = new FindMaximum<Integer>(9, 3, 5);
+		Integer result = findMax.findMax();
 		Assert.assertEquals(9, result, 0);
 	}
 	
 	@Test
 	public void givenMaxNumberAtSeondPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		Integer result = findMax.findMax(3, 9, 5);
+		FindMaximum<Integer> findMax = new FindMaximum<Integer>(3, 9, 5);
+		Integer result = findMax.findMax();
 		Assert.assertEquals(9, result, 0);
 	}
 	
 	@Test
 	public void givenMaxNumberAtThirdPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		Integer result = findMax.findMax(5, 3, 9);
+		FindMaximum<Integer> findMax = new FindMaximum<Integer>(5, 3, 9);
+		Integer result = findMax.findMax();
 		Assert.assertEquals(9, result, 0);
 	}
 	
 	@Test
 	public void givenFloatMaxNumberAtFirstPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		Float result = findMax.findMax(9.0f, 3.0f, 5.0f);
+		FindMaximum<Float> findMax = new FindMaximum<Float>(9.0f, 3.0f, 5.0f);
+		Float result = findMax.findMax();
 		System.out.println(result);
 		Assert.assertEquals(9.0, result, 0.0f);
 	}
 	
 	@Test
 	public void givenFloatMaxNumberAtSeondPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		float result = findMax.findMax(3.0f, 9.0f, 5.0f);
+		FindMaximum<Float> findMax = new FindMaximum<Float>(3.0f, 9.0f, 5.0f);
+		Float result = findMax.findMax();
 		Assert.assertEquals(9.0, result, 0.0f);
 	}
 	
 	@Test
 	public void givenFloatMaxNumberAtThirdPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		Float result = findMax.findMax(5.0f, 3.0f, 9.0f);
+		FindMaximum<Float> findMax = new FindMaximum<Float>(5.0f, 3.0f, 9.0f);
+		Float result = findMax.findMax();
 		Assert.assertEquals(9.0, result, 0.0f);
 	}
 	
 	@Test
 	public void givenStirngMaxNumberAtFirstPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		String result = findMax.findMax("Peach","Apple","Banana");
+		FindMaximum<String> findMax = new FindMaximum<String>("Peach","Apple","Banana");
+		String result = findMax.findMax();
 		System.out.println(result);
 		Assert.assertEquals("Peach", result);
 	}
 	
 	@Test
 	public void givenStringMaxNumberAtSeondPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		String result = findMax.findMax("Apple","Peach","Banana");
+		FindMaximum<String> findMax = new FindMaximum<String>("Apple","Peach","Banana");
+		String result = findMax.findMax();
 		Assert.assertEquals("Peach", result);
 	}
 	
 	@Test
 	public void givenStringMaxNumberAtThirdPostion_ReturnMaximumValue() {
-		FindMaximum findMax = new FindMaximum();
-		String result = findMax.findMax("Apple","Banana","Peach");
+		FindMaximum<String> findMax = new FindMaximum<String>("Apple","Banana","Peach");
+		String result = findMax.findMax();
 		Assert.assertEquals("Peach", result);
 	}
 }
