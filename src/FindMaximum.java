4076aa2616d74fe74c87259bab95bbc00efde0c9
vismaya01 <kvvismaya98@gmail.com>
2020-11-27 20:28:45 +0530

    Added: Find Maximum from three integer

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
 public class FindMaximum {
-
+	public Integer findMax(Integer first, Integer second, Integer thrid) {
+		Integer max = first;
+		if(second.compareTo(max) > 0)
+			max = second;
+		if (thrid.compareTo(max) > 0)
+			max = thrid;
+		return max;
+	}
+	
 }
