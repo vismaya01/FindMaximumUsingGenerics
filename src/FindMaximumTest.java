7cb942b18492e327088f4d2becf031806cbee008
vismaya01 <kvvismaya98@gmail.com>
2020-11-27 20:58:06 +0530

    Added: Test the maximum number Given at first positon

--- src/FindMaximumTest.java
+++ src/FindMaximumTest.java
@@ -2,11 +2,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import org.junit.Assert;
+
 public class FindMaximumTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void givenMaxNumberAtFirstPostion_ReturnMaximumValue() {
+		FindMaximum findMax = new FindMaximum();
+		Integer result = findMax.findMax(9, 3, 5);
+		Assert.assertSame(9, result);
 	}
-
 }
