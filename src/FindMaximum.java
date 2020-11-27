ebde77ed03d753ff307bfb0f4c8a50b5a99bd2ee
vismaya01 <kvvismaya98@gmail.com>
2020-11-27 21:11:04 +0530

    Added: Find Maximum from three Float values

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -10,4 +10,13 @@ public class FindMaximum {
 		return max;
 	}
 	
+	public Float findMax(Float first, Float second, Float third) {
+		Float max = first;
+		if (second.compareTo(max) > 0)
+			max = second;
+		if (third.compareTo(max) > 0)
+			max = third;
+		return max;
+	}
+	
 }
