583592cc5d8a036f71dfb263d627c200d93682d3
vismaya01 <kvvismaya98@gmail.com>
2020-11-27 21:45:01 +0530

    Refactored: Used Generic method for finding maximun

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,33 +1,11 @@
-import java.util.Scanner;
 
 public class FindMaximum {
-	public Integer findMax(Integer first, Integer second, Integer thrid) {
-		Integer max = first;
+	public <E extends Comparable<E>> E findMax(E first, E second, E thrid) {
+		E max = first;
 		if(second.compareTo(max) > 0)
 			max = second;
 		if (thrid.compareTo(max) > 0)
 			max = thrid;
 		return max;
-	}
-	
-	public Float findMax(Float first, Float second, Float third) {
-		Float max = first;
-		if (second.compareTo(max) > 0)
-			max = second;
-		if (third.compareTo(max) > 0)
-			max = third;
-		return max;
-	}
-	
-	public String findMax(String first, String second, String third) {
-		String max = first;
-		if (second.compareTo(max) > 0)
-			max = second;
-		if (third.compareTo(max) > 0)
-			max = third;
-		return max;
-	}
-	
-	
-	
+	}	
 }
